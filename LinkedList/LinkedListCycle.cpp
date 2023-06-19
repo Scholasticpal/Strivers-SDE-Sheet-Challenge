d5f9c6d048c1a5d94afdc3e76c6624d2f46aca44
Anmol Garg <anmolgarg240@gmail.com>
2023-06-19 21:46:34 +0530

    Day 8

---
+++ LinkedList/LinkedListCycle.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/linked-list-cycle/description/
+// Slow-Fast Pointer Approach
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                return true;
+            }
+        }
+        return false;
+    }
+};
