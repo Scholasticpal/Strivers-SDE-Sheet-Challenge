a89c73f0d03695fbdd479b2e7b6170e7fba024dc
Anmol Garg <anmolgarg240@gmail.com>
2023-06-20 19:54:22 +0530

    Day 9

---
+++ Arrays/RemoveDulpicatesFromSortedArray.cpp
@@ -0,0 +1,16 @@
+// https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0;
+        for(int j = 1; j< n; j++){
+            if(nums[i] != nums[j]){
+                i++;
+                nums[i] = nums[j];
+            }
+        }
+        return i+1;
+    }
+};
