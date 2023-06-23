f993e0a0a41622378f0a9da672fc3ee39540839d
Anmol Garg <anmolgarg240@gmail.com>
2023-06-23 16:52:16 +0530

    Day 11

---
+++ Stacks and Queues/MinStack.cpp
@@ -0,0 +1,45 @@
+// https://leetcode.com/problems/min-stack/description/
+
+class MinStack {
+public:
+    stack<pair<int, int>> st;
+    MinStack() {
+    }
+    
+    void push(int val) {
+        int minNum = 0;
+        if(st.empty()){
+            minNum = val;
+        }
+        else{
+            minNum = min(st.top().second, val);
+        }
+        st.push({val, minNum});
+    }
+    
+    void pop() {
+        if(!st.empty()){
+            st.pop();
+        }
+    }
+    
+    int top() {
+        if(st.empty()){
+            return 0;
+        }
+        return st.top().first;
+    }
+    
+    int getMin() {
+        return st.top().second;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
