4949223e2cf79091febac75f0a260db08249eba6
Anmol Garg <anmolgarg240@gmail.com>
2023-07-05 01:35:30 +0530

    Day 15

---
+++ Bit Manipulation/Single_Number_II.cpp
@@ -0,0 +1,17 @@
+//https://leetcode.com/problems/single-number-ii/description/
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ans = 0;
+        for(int i = 0; i< 32; i++){
+            int sum = 0;
+            for(int j = 0; j<nums.size(); j++){
+                sum+= (nums[j]>>i) & 1;
+            }
+            sum = sum%3;
+            ans = ans|(sum<<i);
+        }
+        return ans;
+    }
+};
