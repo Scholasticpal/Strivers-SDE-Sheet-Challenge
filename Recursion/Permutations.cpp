be71a133810027027d4d0e31d2e3d6939dd7fa55
Anmol Garg <anmolgarg240@gmail.com>
2023-07-14 21:25:39 +0530

    Day 17

--- Recursion/Permutations.cpp
+++ Recursion/Permutations.cpp
@@ -2,20 +2,20 @@
 
 class Solution {
 public:
-    void perm(int ind,vector<int>& arr, vector<vector<int>> &ans){
+    void permutations(int ind,vector<int>& arr, vector<vector<int>> &ans){
         if(ind == arr.size()){
             ans.push_back(arr);
             return;
         }
         for(int i=ind;i<arr.size();i++){
                 swap(arr[ind], arr[i]);
-                perm(ind+1, arr, ans);
+                permutations(ind+1, arr, ans);
                 swap(arr[ind], arr[i]);
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
-        perm(0, nums, ans);
+        permutations(0, nums, ans);
         return ans;
     }
 };
